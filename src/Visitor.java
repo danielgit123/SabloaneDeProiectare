ca9e3813c50db8e4e0cf1cdc6e96539b32c15a91
DanieL Preda <predadaniel86@yahoo.com>
2017-11-29 11:15:30 +0200

    Added visitor pattern

---
+++ src/Visitor.java
@@ -0,0 +1,8 @@
+
+public interface Visitor {
+	public void visitImageProxy(Imagine img);
+	public void visitImage(Imagine img);
+	public void visitParagraf(Paragraf paragraf);
+	public void visitTabel(Tabel tabel);
+	public void visitSectiune(Sectiune sectiune);
+}
